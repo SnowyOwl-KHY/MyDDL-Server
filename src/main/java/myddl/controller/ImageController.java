bdef9aa94b1f8226f0b3d8f13c023d14f2df76ae
SnowyOwl-KHY <kehycs@foxmail.com>
2016-05-12 14:15:05 +0800

    fix image bug.

--- src/main/java/myddl/controller/ImageController.java
+++ src/main/java/myddl/controller/ImageController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 
 @Controller
@@ -18,21 +21,25 @@ public class ImageController {
 
     @RequestMapping("/{imageId}")
     @ResponseBody
-    public Object getImage(@PathVariable("imageId") Long imageId) {
-        return ReturnObject.newOKReturnObject(imageMapper.selectByPrimaryKey(imageId));
+    public Object getImage(@PathVariable("imageId") Long imageId, HttpServletResponse response) throws IOException {
+        Image image = imageMapper.selectByPrimaryKey(imageId);
+        byte[] data = image.getData();
+        OutputStream outputStream = response.getOutputStream();
+        outputStream.write(data);
+        return null;
     }
 
     @RequestMapping(value = "/{imageId}", method = RequestMethod.POST)
     @ResponseBody
     public Object modifyImage(@PathVariable("imageId") Long imageId,
-                              @RequestParam("data") byte[] data) {
+                              @RequestBody byte[] data) {
         imageMapper.updateByPrimaryKeySelective(new Image(imageId, data));
         return ReturnObject.EXECUTION_SUCCESS;
     }
 
     @RequestMapping(value = "", method = RequestMethod.PUT)
     @ResponseBody
-    public Object addImage(@RequestParam("data") byte[] data) {
+    public Object addImage(@RequestBody byte[] data) {
         Image image = new Image(null, data);
         imageMapper.insertSelective(image);
         HashMap<String, Long> result = new HashMap<>();
