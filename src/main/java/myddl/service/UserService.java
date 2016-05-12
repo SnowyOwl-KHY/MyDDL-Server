4389f9a878bb83766d04bf9f54b2d5967e44b412
SnowyOwl-KHY <kehycs@foxmail.com>
2016-05-12 22:04:47 +0800

    Trying fix bug.

--- src/main/java/myddl/service/UserService.java
+++ src/main/java/myddl/service/UserService.java
@@ -3,12 +3,13 @@ package myddl.service;
 import myddl.entity.UserInfo;
 import myddl.entity.UserPassword;
 import myddl.returnobject.UserRO;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserService {
 
     Long login(String username, String password);
 
-    UserRO getUser(Long userId);
+    UserRO getUser(@Param("userId") Long userId);
 
     void modifyUser(UserPassword userPassword, UserInfo userInfo);
 
